5c26619c7b7804c53a94f2821f5752e7ed8569e7
kubabukry <95041821+kubabukry@users.noreply.github.com>
2022-10-25 22:05:10 +0200

    Revert "task1"

--- src/main/java/com/example/survey/repository/RegisteredUserRepository.java
+++ src/main/java/com/example/survey/repository/RegisteredUserRepository.java
@@ -3,7 +3,6 @@ package com.example.survey.repository;
 import com.example.survey.model.RegisteredUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-// kom
 
 @Repository     //not necessary as JpaRepository includes it
 public interface RegisteredUserRepository extends JpaRepository<RegisteredUser, Long> {
