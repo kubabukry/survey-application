01bb2aec0b1b5a12cc447db96c19bcb5d800dedc
kubabukry <95041821+kubabukry@users.noreply.github.com>
2022-11-19 17:36:52 +0100

    basic repositories added

--- src/main/java/com/example/survey/repository/CredentialsRepository.java
+++ src/main/java/com/example/survey/repository/CredentialsRepository.java
@@ -1,10 +1,9 @@
 package com.example.survey.repository;
 
-import com.example.survey.model.Role;
+import com.example.survey.model.Credentials;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
+public interface CredentialsRepository extends JpaRepository<Credentials, Long> {
 }
--- src/main/java/com/example/survey/repository/SurveyTemplateRepository.java
+++ src/main/java/com/example/survey/repository/SurveyTemplateRepository.java
@@ -1,10 +1,9 @@
 package com.example.survey.repository;
 
-import com.example.survey.model.Role;
+import com.example.survey.model.SurveyTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
+public interface SurveyTemplateRepository extends JpaRepository<SurveyTemplate, Long> {
 }
