8dc503cbe7737b8c1e4cd6205163f5d1ac0d9a5a
Radek Grędel <radoslaw.gredel@student.pk.edu.pl>
2023-01-07 18:01:08 +0100

    Adds cors config for disabled security

---
+++ src/main/java/com/example/survey/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.example.survey.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ConditionalOnProperty(prefix = "survey", name = "security", havingValue = "false")
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
