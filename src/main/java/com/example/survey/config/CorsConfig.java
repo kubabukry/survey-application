9b470909f8168fc165e528f0622cc57c53d2a70e
Radek Grędel <radoslaw.gredel@student.pk.edu.pl>
2023-01-08 16:13:11 +0100

    fixes cors for PUT requests

--- src/main/java/com/example/survey/config/CorsConfig.java
+++ src/main/java/com/example/survey/config/CorsConfig.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
     }
 }
